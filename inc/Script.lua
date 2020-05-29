8873d716fe55164a95737eb209fec1d3800cfd82
AHMEDFGTH <65931554+AHMEDFGTH@users.noreply.github.com>
2020-05-29 16:06:45 +0300

    Update Script.lua

--- inc/Script.lua
+++ inc/Script.lua
@@ -2200,7 +2200,7 @@ end
 
 if (MsgText[1] == 'تحديث السورس' or MsgText[1] == 'تحديث السورس 🔂') then
 if not msg.SudoBase then return "♦️*│*هذا الامر يخص {المطور الاساسي} فقط  \n💥" end
-local GetVerison = https.request('https://github.com/FHAIWIH/FHAIWIH.github.io/GetVersion.txt') or 0
+local GetVerison = https.request('https://github.com/AHMEDFGTH/AHMED.github.io/GetVersion.txt') or 0
 if GetVerison > version then
 UpdateSourceStart = true
 sendMsg(msg.chat_id_,msg.id_,'🔛*│* يوجد تحديث جديد الان \n📡*│* جاري تنزيل وتثبيت التحديث  ...')
